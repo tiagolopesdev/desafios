ad923e10c497ce5681a08675b26565b55f0976b4
tiagoLopes <saxtiago14@gmail.com>
2022-04-28 20:25:45 -0300

    Desafio gincana: pegando elementos e retirando desde o ultimo.

---
+++ src/main/java/DIO/OrdenaçãoFiltros/GincanaAcampamento.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DIO.OrdenaçãoFiltros;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+/**
+ *
+ * @author tiagolopes
+ */
+public class GincanaAcampamento {
+
+    public static void main(String[] args) throws IOException {
+
+        //Scanner sc = new Scanner(System.in);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(reader.readLine());
+
+        Integer qntRepet = Integer.parseInt(st.nextToken());
+
+//        System.out.println(qntRepet);
+        String nextElement;
+        String[] namesNumbers;
+        Integer number;
+        List<String> namesList = new ArrayList<>();
+        List<Integer> numbersList = new ArrayList<>();
+        boolean loop = true;
+        //while (loop == true) {
+        for (int i = 0; i < qntRepet; i++) {
+            nextElement = reader.readLine();
+            namesNumbers = nextElement.split(" ");
+            number = Integer.parseInt(namesNumbers[1]);
+            numbersList.add(number);
+            namesList.add(namesNumbers[0]);
+        }
+        System.out.println("Tamanho lista: " + numbersList.size());
+        Integer qntRepetNumber = numbersList.size() - 1;
+        //System.out.println(qntRepetNumber);
+        //qntRepetNumber--;
+
+        Integer getIndex = 0;
+        for (int i = qntRepetNumber; i >= 0; i--) {
+            if (i != 0) {
+                numbersList.remove(i);
+            } else {
+                getIndex = i;
+            }
+        }
+        System.out.println("Ultimo elemento: " + numbersList.get(getIndex)+"\nGanhadora: "+namesList.get(getIndex));
+        //}
+
+    }
+}
