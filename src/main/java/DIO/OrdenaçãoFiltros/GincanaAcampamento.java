8b49a6717086ee3e4dfea9adece352ea9e2de7bc
tiagoLopes <saxtiago14@gmail.com>
2022-05-05 21:13:24 -0300

    Gincana no acampamento: corrido, falta exebir outros resultados.

--- src/main/java/DIO/OrdenaçãoFiltros/GincanaAcampamento.java
+++ src/main/java/DIO/OrdenaçãoFiltros/GincanaAcampamento.java
@@ -10,7 +10,6 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Scanner;
 import java.util.StringTokenizer;
 
 /**
@@ -21,46 +20,52 @@ public class GincanaAcampamento {
 
     public static void main(String[] args) throws IOException {
 
-        //Scanner sc = new Scanner(System.in);
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(reader.readLine());
 
         Integer qntRepet = Integer.parseInt(st.nextToken());
 
-//        System.out.println(qntRepet);
         String nextElement;
         String[] namesNumbers;
         Integer number;
+        Integer qntCaracteres;
         List<String> namesList = new ArrayList<>();
-        List<Integer> numbersList = new ArrayList<>();                
+        List<Integer> numbersList = new ArrayList<>();
         for (int i = 0; i < qntRepet; i++) {
             nextElement = reader.readLine();
             namesNumbers = nextElement.split(" ");
             number = Integer.parseInt(namesNumbers[1]);
-            numbersList.add(number);            
-            namesList.add(namesNumbers[0]);
-        }
-        System.out.println("Tamanho lista: " + numbersList.size());
-        Integer qntRepetNumber = numbersList.size() - 1;        
-
-        Integer getIndex = 0;
-        for (int i = qntRepetNumber; i >= 0; i--) {
-            if (i != 0) {
-                if (numbersList.get(i) % 2 == 0) {
-                    numbersList.remove(i);
-                    namesList.remove(i);
-                    Collections.reverse(numbersList);
-                    Collections.reverse(namesList);
-                } else {
-                    namesList.remove(i);
-                    numbersList.remove(i);
-                }
-            } else {
-                getIndex = i;
+            qntCaracteres = nextElement.indexOf(" ");
+            Character.isAlphabetic(qntCaracteres);
+            if (qntCaracteres < 30 && !namesNumbers[0].contains("_")) {
+                numbersList.add(number);
+                namesList.add(namesNumbers[0]);
             }
         }
-        System.out.println("Ultimo elemento: " + numbersList.get(getIndex) + "\nGanhadora: " + namesList.get(getIndex));
-        //}
+        Integer qntInverse = 0;
+        if (qntRepet != 0) {
+            for (int i = numbersList.size() - 1; i >= 0; i--) {
+                if (i > 0) {
+                    if (numbersList.get(numbersList.size() - 1) % 2 == 0) {
+                        numbersList.remove(numbersList.size() - 1);
+                        namesList.remove(namesList.size() - 1);
+                        Collections.reverse(numbersList);
+                        Collections.reverse(namesList);
+                        qntInverse++;
+                    } else {
+                        namesList.remove(namesList.size() - 1);
+                        numbersList.remove(numbersList.size() - 1);
+                        if (qntInverse > 0) {
+                            Collections.reverse(numbersList);
+                            Collections.reverse(namesList);
+                            qntInverse=0;
+                        }
+                    }
+                }
+            }
+            String result = namesList.get(0);
+            System.out.println("Vencedor(a): " + result);
+        }
 
     }
 }
