df6d74c9451fa0e4d800122b965bf370bb5e5063
tiagoLopes <saxtiago14@gmail.com>
2022-05-27 08:05:51 -0300

    Finalizando desafio de médio do Nitro.

---
+++ src/main/java/Teste.java
@@ -0,0 +1,38 @@
+
+import java.util.Scanner;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+/**
+ *
+ * @author tiagolopes
+ */
+public class Teste {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Integer qntLoop = sc.nextInt();
+
+        Integer numbers[] = new Integer[qntLoop];
+        Integer somaNumbers = 0;
+        Integer qntAlunos = 0;
+        
+        for (int i = 0; i < qntLoop; i++) {
+            numbers[i] = sc.nextInt();
+        }
+        for (int i = 0; i < qntLoop; i++) {
+            somaNumbers += numbers[i];
+        }
+        somaNumbers /= qntLoop;        
+        
+        for (int i = 0; i < qntLoop; i++) {
+            if (numbers[i] > somaNumbers) {
+                qntAlunos++;
+            }
+        }
+        System.out.println("Qnt Alunos: "+ qntAlunos);
+    }
+}
