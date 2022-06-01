01a96961e9468065caa531b3cfa7b3c15ac3bfed
Tiago Lopes <saxtiago14@gmail.com>
2022-06-01 20:31:04 -0300

    Finalizando código do desafio melhor calculadora.

--- src/main/java/Nitro/MelhorCalculadora.java
+++ src/main/java/Nitro/MelhorCalculadora.java
@@ -19,30 +19,17 @@ public class MelhorCalculadora {
 
         char operacao = scan.next().charAt(0);
         BigDecimal numberOne = scan.nextBigDecimal();
-        BigDecimal numberTwo = scan.nextBigDecimal();
-
-//        if (operacao.contains(''+')) {;
-//            System.out.println(numberOne.add(numberTwo));
-//        } else if (operacao.contains("-")) {
-//            System.out.println(numberOne.subtract(numberTwo));
-//        } else if (operacao.contains("/")) {
-//            System.out.println(numberOne.divide(numberTwo, 1, RoundingMode.UP));
-//        } else if (operacao.contains("*")) {
-//            System.out.println(numberOne.multiply(numberTwo));
-//        }
-        switch (operacao) {
-            case '+':
-                System.out.println(numberOne.add(numberTwo));
-                break;
-            case '-':
-                System.out.println(numberOne.subtract(numberTwo));
-                break;
-            case '/':
-                System.out.println(numberOne.divide(numberTwo, 1, RoundingMode.UP));
-                break;
-            case '*':
-                System.out.println(numberOne.multiply(numberTwo));
-                break;
+        BigDecimal numberTwo = scan.nextBigDecimal();        
+        
+        if (operacao == '+') {
+            System.out.println(numberOne.add(numberTwo));
+        } else if (operacao == '-') {
+            System.out.println(numberOne.subtract(numberTwo));
+        } else if (operacao == '*') {
+            System.out.println(numberOne.multiply(numberTwo));
+        } else if (operacao == '/') {
+            System.out.println(numberOne.divide(numberTwo,
+                    1, RoundingMode.UP));
         }
     }
 }
