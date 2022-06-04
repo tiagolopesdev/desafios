fe74a60f5e47d9327781acc9ddd3ad770bc3983e
tiagoLopes <saxtiago14@gmail.com>
2022-06-03 22:19:38 -0300

    Finalizando código do desafio separando em grupo, Nitro.

---
+++ src/main/java/Nitro/SeparandoGrupo.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Nitro;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author tiagolopes
+ */
+public class SeparandoGrupo {
+
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+        
+        String name = scan.next();
+        
+        Integer qntWorldName = name.length();
+
+        if (qntWorldName <= 4) {
+            System.out.println("GRUPO A");
+        } else if (qntWorldName >= 5 && qntWorldName <= 10) {
+            System.out.println("GRUPO B");            
+        } else if (qntWorldName > 10) {
+            System.out.println("GRUPO C");                        
+        }
+    }
+}
