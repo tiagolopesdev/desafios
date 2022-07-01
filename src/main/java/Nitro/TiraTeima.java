eeaf5b85926eee2b9e9db21fa1093f0ae1e3cf1f
tiagoLopes <saxtiago14@gmail.com>
2022-06-30 22:58:32 -0300

    Refatorando - criando casos de teste para tira-teima.

--- src/main/java/Nitro/TiraTeima.java
+++ src/main/java/Nitro/TiraTeima.java
@@ -12,18 +12,27 @@ import java.util.*;
  */
 public class TiraTeima {
 
+    long defaultPositionX = 432;
+    long defaultPositionY = 432;
+    String result = null;
+
+    String exibiResult(long positionX, long positionY) {
+        if (verificaInputs(positionX, defaultPositionX)
+                && verificaInputs(positionY, defaultPositionY)) {
+            result = "dentro";
+        } else {
+            result = "fora";
+        }
+        return result;
+    }
+
+    boolean verificaInputs(long positionInput, long defaultPosition) {
+        return positionInput >= 0 && positionInput <= defaultPosition;
+    }
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
-        long positionX = scan.nextLong();
-        long positionY = scan.nextLong();
-        long defaultPositionX = 432;
-        long defaultPositionY = 432;
-
-        if ((positionX >= 0 && positionX <= defaultPositionX) && (positionY >= 0 && positionY <= defaultPositionY)) {            
-            System.out.print("dentro");
-        } else {
-            System.out.print("fora");
-        }
+        TiraTeima tiraTeima = new TiraTeima();
+        System.out.println(tiraTeima.exibiResult(scan.nextLong(), scan.nextLong()));
     }
 }
